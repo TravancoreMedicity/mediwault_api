6f97634c6c89153e6a1a8c74adca2debe48b4207
IT Medicity <itmedicity@tmc.ac.in>
2024-10-18 10:51:28 +0530

    changes

--- api/docMaster/docMaster.controller.js
+++ api/docMaster/docMaster.controller.js
@@ -1,164 +1,165 @@
-const logger = require('../../logger/logger');
+const logger = require("../../logger/logger");
 
 const {
-    insertDocMaster,
-    checkDocNameDuplicate,
-    getDocMaster,
-    getDocMasterById,
-    getDocMasterLikeName,
-    getDocNonSecure,
-    getDocSecureOnly,
-    inCrementDocSerialNumber
-} = require('./docMaster.service');
+  insertDocMaster,
+  checkDocNameDuplicate,
+  getDocMaster,
+  getDocMasterById,
+  getDocMasterLikeName,
+  getDocNonSecure,
+  getDocSecureOnly,
+  inCrementDocSerialNumber,
+} = require("./docMaster.service");
 
-const { uploadFile } = require('../multer.config/FileuploadConfig');
+const { uploadFile } = require("../multer.config/FileuploadConfig");
 
 module.exports = {
+  insertDocMaster: (req, res) => {
+    // console.log(JSON.stringify(req.body))
+    uploadFile(req, res, (err) => {
+      //   const body = req.body;
+      //   console.log(req.files);
 
-    insertDocMaster: (req, res) => {
+      const body = JSON.parse(JSON.parse(JSON.stringify(req.body))?.postData);
+      const fileInformation = (req.files?.length > 0 && req.files) || [];
+      const postUploadFileData = fileInformation?.map((el) => {
+        return {
+          ...el,
+          docID: body.docID,
+          docNumber: body.docNumber,
+        };
+      });
+      console.log(postUploadFileData);
+      //   console.log(body);
 
-        // console.log(JSON.stringify(req.body))
-        uploadFile(req, res, (err) => {
-            const body = req.body
+      if (err) {
+        logger.error(err);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error",
+        });
+      }
 
-            // console.log(body)
-            // const postData = JSON.parse(JSON.parse(JSON.stringify(body))?.postData);
-            // console.log(postData)
-            // if (err) {
-            //     logger.error(err)
-            //     return res.status(500).json({
-            //         success: 0,
-            //         message: 'Database connection error'
-            //     })
-            // }
+      // console.log(req.body)
+      // console.log(req.file)
+      // console.log(req.files)
 
-            // console.log(req.body)
-            // console.log(req.file)
-            // console.log(req.files)
-
-            // const name = body?.docName?.trim()
-            // checkDocNameDuplicate(name, (err, results) => {
-            //     if (err) {
-            //         logger.error(err)
-            //         return res.status(500).json({
-            //             success: 0,
-            //             message: 'Database connection error'
-            //         })
-            //     }
-            //     if (results?.length > 0) {
-            //         return res.status(200).json({
-            //             success: 2,
-            //             message: 'Doc name already exist'
-            //         })
-            //     }
-            //     if (results?.length === 0) {
-            //         insertDocMaster(body, (err, results) => {
-            //             if (err) {
-            //                 logger.error(err)
-            //                 return res.status(500).json({
-            //                     success: 0,
-            //                     message: 'Database connection error'
-            //                 })
-            //             }
-            //             if (results) {
-            //                 inCrementDocSerialNumber((err, results) => logger.error(err))
-            //                 return res.status(200).json({
-            //                     success: 1,
-            //                     message: 'Record Inserted successfully',
-            //                 })
-            //             }
-            //         })
-            //     }
-            // })
-
-
-
-
-        })
-
-
-
-    },
-    getDocMaster: (req, res) => {
-        getDocMaster((err, results) => {
+      const name = body?.docName?.trim();
+      checkDocNameDuplicate(name, (err, results) => {
+        if (err) {
+          logger.error(err);
+          return res.status(500).json({
+            success: 0,
+            message: "Database connection error",
+          });
+        }
+        if (results?.length > 0) {
+          return res.status(200).json({
+            success: 2,
+            message: "Doc name already exist",
+          });
+        }
+        if (results?.length === 0) {
+          insertDocMaster(body, (err, results) => {
             if (err) {
-                logger.error(err)
-                return res.status(500).json({
-                    success: 0,
-                    message: 'Database connection error'
-                })
+              logger.error(err);
+              return res.status(500).json({
+                success: 0,
+                message: "Database connection error",
+              });
             }
-            return res.status(200).json({
+            if (results) {
+              inCrementDocSerialNumber((err, results) => logger.error(err));
+              return res.status(200).json({
                 success: 1,
-                message: 'success',
-                data: results
-            })
-        })
-    },
-    getDocMasterById: (req, res) => {
-        const id = req.params.id
-        getDocMasterById(id, (err, results) => {
-            if (err) {
-                logger.error(err)
-                return res.status(500).json({
-                    success: 0,
-                    message: 'Database connection error'
-                })
+                message: "Record Inserted successfully",
+              });
             }
-            return res.status(200).json({
-                success: 1,
-                message: 'success',
-                data: results
-            })
-        })
-    },
-    getDocMasterLikeName: (req, res) => {
-        const body = req.body
-        getDocMasterLikeName(body, (err, results) => {
-            if (err) {
-                logger.error(err)
-                return res.status(500).json({
-                    success: 0,
-                    message: 'Database connection error'
-                })
-            }
-            return res.status(200).json({
-                success: 1,
-                message: 'success',
-                data: results
-            })
-        })
-    },
-    getDocNonSecure: (req, res) => {
-        getDocNonSecure((err, results) => {
-            if (err) {
-                logger.error(err)
-                return res.status(500).json({
-                    success: 0,
-                    message: 'Database connection error'
-                })
-            }
-            return res.status(200).json({
-                success: 1,
-                message: 'success',
-                data: results
-            })
-        })
-    },
-    getDocSecureOnly: (req, res) => {
-        getDocSecureOnly((err, results) => {
-            if (err) {
-                logger.error(err)
-                return res.status(500).json({
-                    success: 0,
-                    message: 'Database connection error'
-                })
-            }
-            return res.status(200).json({
-                success: 1,
-                message: 'success',
-                data: results
-            })
-        })
-    }
-}
+          });
+        }
+      });
+    });
+  },
+  getDocMaster: (req, res) => {
+    getDocMaster((err, results) => {
+      if (err) {
+        logger.error(err);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error",
+        });
+      }
+      return res.status(200).json({
+        success: 1,
+        message: "success",
+        data: results,
+      });
+    });
+  },
+  getDocMasterById: (req, res) => {
+    const id = req.params.id;
+    getDocMasterById(id, (err, results) => {
+      if (err) {
+        logger.error(err);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error",
+        });
+      }
+      return res.status(200).json({
+        success: 1,
+        message: "success",
+        data: results,
+      });
+    });
+  },
+  getDocMasterLikeName: (req, res) => {
+    const body = req.body;
+    getDocMasterLikeName(body, (err, results) => {
+      if (err) {
+        logger.error(err);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error",
+        });
+      }
+      return res.status(200).json({
+        success: 1,
+        message: "success",
+        data: results,
+      });
+    });
+  },
+  getDocNonSecure: (req, res) => {
+    getDocNonSecure((err, results) => {
+      if (err) {
+        logger.error(err);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error",
+        });
+      }
+      return res.status(200).json({
+        success: 1,
+        message: "success",
+        data: results,
+      });
+    });
+  },
+  getDocSecureOnly: (req, res) => {
+    getDocSecureOnly((err, results) => {
+      if (err) {
+        logger.error(err);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error",
+        });
+      }
+      return res.status(200).json({
+        success: 1,
+        message: "success",
+        data: results,
+      });
+    });
+  },
+};
--- app.js
+++ app.js
@@ -1,61 +1,61 @@
 // @ts-nocheck
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const logger = require('./logger/logger')
-const mysql = require('mysql2/promise')
-const jwt = require('jsonwebtoken')
-const axios = require('axios');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("./logger/logger");
+const mysql = require("mysql2/promise");
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((err, req, res, next) => {
-    logger.error(`Unhandled error: ${err.message}`);
-    res.status(500).send('Something broke!');
+  logger.error(`Unhandled error: ${err.message}`);
+  res.status(500).json({
+    message: "unexpected error was occured",
+  });
 });
 
-const { generateOTP } = require('./api/usermanagment/user.controller')
+const { generateOTP } = require("./api/usermanagment/user.controller");
 
-const userRegistration = require('./api/usermanagment/user.route')
-const selectCmp = require('./api/selectComponets/selectCmp.route')
-const docTypeMaster = require('./api/documentTypeMaster/docTypeMaster.route')
-const subTypeMaster = require('./api/subTypeMaster/subTypeMaster.route')
-const categoryMaster = require('./api/documentCategory/docCategory.route')
-const docSubCategory = require('./api/docSubCategoryName/docSubCategory.route')
-const docGroup = require('./api/docGroupMaster/docGroup.route')
-const institutionType = require('./api/InstituteTyeMaster/instituteType.route')
-const institutionMaster = require('./api/InstitutionMaster/institutionMaster.route')
-const courseType = require('./api/courseType/courseType.route')
-const courseMaster = require('./api/courseMaster/courseMater.route')
+const userRegistration = require("./api/usermanagment/user.route");
+const selectCmp = require("./api/selectComponets/selectCmp.route");
+const docTypeMaster = require("./api/documentTypeMaster/docTypeMaster.route");
+const subTypeMaster = require("./api/subTypeMaster/subTypeMaster.route");
+const categoryMaster = require("./api/documentCategory/docCategory.route");
+const docSubCategory = require("./api/docSubCategoryName/docSubCategory.route");
+const docGroup = require("./api/docGroupMaster/docGroup.route");
+const institutionType = require("./api/InstituteTyeMaster/instituteType.route");
+const institutionMaster = require("./api/InstitutionMaster/institutionMaster.route");
+const courseType = require("./api/courseType/courseType.route");
+const courseMaster = require("./api/courseMaster/courseMater.route");
 
-const docMaster = require('./api/docMaster/docMaster.route')
+const docMaster = require("./api/docMaster/docMaster.route");
 
-app.use('/api/docMaster', docMaster)
+app.use("/api/docMaster", docMaster);
 
-
-app.get('/api/generateOTP/:id', generateOTP)  // generate OTP function
-app.use('/api/user', userRegistration)
-app.use('/api/selectComponets', selectCmp)
-app.use('/api/documentTypeMaster', docTypeMaster)
-app.use('/api/subTypeMaster', subTypeMaster)
-app.use('/api/documentCategory', categoryMaster)
-app.use('/api/docSubCategoryName', docSubCategory)
-app.use('/api/docGroupMaster', docGroup)
-app.use('/api/instituteType', institutionType)
-app.use('/api/institutionMaster', institutionMaster)
-app.use('/api/courseType', courseType)
-app.use('/api/courseMaster', courseMaster)
+app.get("/api/generateOTP/:id", generateOTP); // generate OTP function
+app.use("/api/user", userRegistration);
+app.use("/api/selectComponets", selectCmp);
+app.use("/api/documentTypeMaster", docTypeMaster);
+app.use("/api/subTypeMaster", subTypeMaster);
+app.use("/api/documentCategory", categoryMaster);
+app.use("/api/docSubCategoryName", docSubCategory);
+app.use("/api/docGroupMaster", docGroup);
+app.use("/api/instituteType", institutionType);
+app.use("/api/institutionMaster", institutionMaster);
+app.use("/api/courseType", courseType);
+app.use("/api/courseMaster", courseMaster);
 
 // General error handling middleware
-
 
 const port = process.env.PORT || 58888;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
