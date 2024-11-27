4e9ba1a99f5d5e1b7f2246c34837c3e048730f6d
IT Medicity <itmedicity@tmc.ac.in>
2024-11-27 11:00:29 +0530

    changes

--- api/usermanagment/user.controller.js
+++ api/usermanagment/user.controller.js
@@ -1,315 +1,319 @@
 // @ts-nocheck
-require('dotenv').config();
-const { default: axios } = require('axios');
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const { default: axios } = require("axios");
+const jwt = require("jsonwebtoken");
 const {
-    insertUser,
-    editUser,
-    deleteUser,
-    getUser,
-    getAllUser,
-    mobileExist,
-    emailExist,
-    insertOTP,
-    verifyOTP,
-    insertRefreshToken,
-    getRefershToken
-} = require('./user.service');
+  insertUser,
+  editUser,
+  deleteUser,
+  getUser,
+  getAllUser,
+  mobileExist,
+  emailExist,
+  insertOTP,
+  verifyOTP,
+  insertRefreshToken,
+  getRefershToken,
+} = require("./user.service");
 
-const { addHours, format } = require('date-fns');
-const logger = require('../../logger/logger');
-const { generateAccessToken, generateRefreshToken } = require('../helperFunction/HelperFunction');
-
+const { addHours, format } = require("date-fns");
+const logger = require("../../logger/logger");
+const {
+  generateAccessToken,
+  generateRefreshToken,
+} = require("../helperFunction/HelperFunction");
 
 module.exports = {
-    insertUser: (req, res) => {
-        const body = req.body
+  insertUser: (req, res) => {
+    const body = req.body;
 
-        mobileExist(body.mobile, (error, results) => {
+    mobileExist(body.mobile, (error, results) => {
+      if (error) {
+        logger.error(error);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error",
+        });
+      }
+
+      if (results?.length > 0) {
+        return res.status(200).json({
+          success: 2,
+          message: "Mobile number already exist",
+        });
+      }
+
+      emailExist(body.email, (error, results) => {
+        if (error) {
+          logger.error(error);
+          return res.status(500).json({
+            success: 0,
+            message: "Database connection error",
+          });
+        }
+
+        if (results?.length > 0) {
+          return res.status(200).json({
+            success: 2,
+            message: "Email already exist",
+          });
+        }
+
+        if (results?.length === 0) {
+          insertUser(body, (error, results) => {
             if (error) {
-                logger.error(error)
-                return res.status(500).json({
-                    success: 0,
-                    message: "Database connection error"
-                });
-            }
-
-            if (results?.length > 0) {
-                return res.status(200).json({
-                    success: 2,
-                    message: "Mobile number already exist"
-                });
-            }
-
-            emailExist(body.email, (error, results) => {
-                if (error) {
-                    logger.error(error)
-                    return res.status(500).json({
-                        success: 0,
-                        message: "Database connection error"
-                    });
-                }
-
-                if (results?.length > 0) {
-                    return res.status(200).json({
-                        success: 2,
-                        message: "Email already exist"
-                    });
-                }
-
-                if (results?.length === 0) {
-                    insertUser(body, (error, results) => {
-                        if (error) {
-                            return res.status(500).json({
-                                success: 0,
-                                message: "Database connection error"
-                            });
-                        }
-                        return res.status(200).json({
-                            success: 1,
-                            message: "User created successfully"
-                        });
-                    });
-                }
-
-            })
-        })
-    },
-    editUser: (req, res) => {
-        const body = req.body
-        editUser(body, (error, results) => {
-            if (error) {
-                logger.error(error)
-                return res.status(500).json({
-                    success: 0,
-                    message: "Database connection error"
-                });
+              return res.status(500).json({
+                success: 0,
+                message: "Database connection error",
+              });
             }
             return res.status(200).json({
-                success: 1,
-                data: results
+              success: 1,
+              message: "User created successfully",
             });
+          });
+        }
+      });
+    });
+  },
+  editUser: (req, res) => {
+    const body = req.body;
+    editUser(body, (error, results) => {
+      if (error) {
+        logger.error(error);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error",
         });
-    },
-    deleteUser: (req, res) => {
-        const id = req.params.id
-        deleteUser(id, (error, results) => {
-            if (error) {
-                logger.error(error)
-                return res.status(500).json({
-                    success: 0,
-                    message: "Database connection error"
-                });
-            }
-            return res.status(200).json({
-                success: 1,
-                data: results
+      }
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  deleteUser: (req, res) => {
+    const id = req.params.id;
+    deleteUser(id, (error, results) => {
+      if (error) {
+        logger.error(error);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error",
+        });
+      }
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  getUser: (req, res) => {
+    const id = req.params.id;
+    getUser(id, (error, results) => {
+      if (error) {
+        logger.error(error);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error",
+        });
+      }
+
+      if (results?.length === 0) {
+        return res.status(200).json({
+          success: 2,
+          message: "no data",
+        });
+      }
+
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  getAllUser: (req, res) => {
+    getAllUser((error, results) => {
+      if (error) {
+        logger.error(error);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error",
+        });
+      }
+
+      if (results?.length === 0) {
+        return res.status(200).json({
+          success: 2,
+          message: "no data",
+        });
+      }
+
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  generateOTP: async (req, res) => {
+    const mobileNumber = req.params.id;
+    const trimmedNumber = mobileNumber.slice(2);
+    // First check mobile number registerd or not
+    mobileExist(trimmedNumber, (error, results) => {
+      if (error) {
+        logger.error(error);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error",
+        });
+      }
+
+      if (results.length === 0) {
+        return res.status(200).json({
+          success: 1,
+          message: "Mobile number not registered",
+        });
+      }
+
+      if (results.length > 0) {
+        const otp = Math.floor(100000 + Math.random() * 900000);
+        insertOTP({ mobile: trimmedNumber, otp: otp }, (error, results) => {
+          if (error) {
+            logger.error(error);
+            return res.status(500).json({
+              success: 0,
+              message: "Database connection error",
             });
-        });
-    },
-    getUser: (req, res) => {
-        const id = req.params.id
-        getUser(id, (error, results) => {
-            if (error) {
-                logger.error(error)
-                return res.status(500).json({
-                    success: 0,
-                    message: "Database connection error"
-                });
-            }
-
-            if (results?.length === 0) {
+          }
+          if (results) {
+            axios
+              .get(
+                `https://sapteleservices.com/SMS_API/sendsms.php?username=Tmc_medicity&password=c9e780&sendername=TMDCTY&mobile=${mobileNumber}&template_id=1407162012178109509&message=Your+Medicity+App+OTP+code:+${otp}+DuHTEah22dE.Travancore+Medicity+.&routetype=1`
+              )
+              .then((response) => {
                 return res.status(200).json({
-                    success: 2,
-                    message: "no data"
+                  success: 2,
+                  message: "OTP sent successfully",
                 });
+              })
+              .catch((error) => {
+                logger.error(error);
+                return res.status(200).json({
+                  success: 3,
+                  message: "Error in sending OTP,Please try again",
+                });
+              });
+          }
+        });
+      }
+    });
+  },
+  verifyOTPandLogin: async (req, res) => {
+    const body = req.body;
+
+    verifyOTP(body, (error, results) => {
+      if (error) {
+        logger.error(error);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error",
+        });
+      }
+      if (results.length === 0) {
+        return res.status(200).json({
+          success: 1,
+          message: "Incorrect OTP",
+        });
+      }
+      if (results.length > 0) {
+        const userData = results[0];
+        const { user_slno, name, login_type } = userData;
+
+        const accessToken = generateAccessToken(userData);
+        const refreshToken = generateRefreshToken(user_slno);
+
+        // insert the refresh token
+        insertRefreshToken(
+          { user_slno, refresh_token: refreshToken },
+          (error, results) => {
+            if (error) {
+              logger.error(error);
+              return res.status(500).json({
+                success: 0,
+                message: "Database connection error",
+              });
             }
 
-            return res.status(200).json({
-                success: 1,
-                data: results
+            if (results) {
+              const returnData = {
+                user_slno,
+                name,
+                accessToken,
+                login_type,
+              };
+
+              res.cookie("accessToken", accessToken, {
+                httpOnly: false,
+                // secure: true,
+                // maxAge: 900000, // 15 min
+                maxAge: 60000, // 1 min
+                sameSite: "strict",
+              });
+
+              // res.cookie("refreshToken", refreshToken, {
+              //     httpOnly: true,
+              //     secure: true,
+              //     maxAge: 86400000, // 24 hour
+              //     sameSite: "strict",
+              // });
+
+              res.json({
+                success: 2,
+                userInfo: JSON.stringify(returnData),
+                message: "OTP verified successfully",
+              });
+            }
+          }
+        );
+      }
+    });
+  },
+  getRefershToken: (req, res) => {
+    const id = req.params.id;
+    getRefershToken(id, (error, results) => {
+      if (error) {
+        logger.error(error);
+        return res.status(403).json({ message: "Invalid refresh token" });
+      }
+      if (results.length === 0) {
+        return res.status(403).json({ message: "Invalid refresh token" });
+      }
+      if (results.length > 0) {
+        // console.log(results[0].token)
+        const refreshToken = results[0].token;
+
+        jwt.verify(
+          refreshToken,
+          process.env.REFRESH_TOKEN_SECRET,
+          (err, user) => {
+            if (err) {
+              return res.status(403).json({ message: "Invalid refresh token" });
+            }
+
+            const newAccessToken = generateAccessToken({
+              id: id,
             });
-        });
-    },
-    getAllUser: (req, res) => {
-        getAllUser((error, results) => {
-            if (error) {
-                logger.error(error)
-                return res.status(500).json({
-                    success: 0,
-                    message: "Database connection error"
-                });
-            }
-
-            if (results?.length === 0) {
-                return res.status(200).json({
-                    success: 2,
-                    message: "no data"
-                });
-            }
-
-            return res.status(200).json({
-                success: 1,
-                data: results
+            // console.log(newAccessToken)
+            res.cookie("accessToken", newAccessToken, {
+              httpOnly: true,
+              secure: true,
+              // maxAge: 900000, // 15 min
+              maxAge: 60000, // 1 min
+              sameSite: "strict",
             });
-        });
-    },
-    generateOTP: async (req, res) => {
-        const mobileNumber = req.params.id;
-        const trimmedNumber = mobileNumber.slice(2);
-        // First check mobile number registerd or not
-        mobileExist(trimmedNumber, (error, results) => {
-            if (error) {
-                logger.error(error)
-                return res.status(500).json({
-                    success: 0,
-                    message: "Database connection error"
-                });
-            }
-
-            if (results.length === 0) {
-                return res.status(200).json({
-                    success: 1,
-                    message: "Mobile number not registered"
-                });
-            }
-
-            if (results.length > 0) {
-                const otp = Math.floor(100000 + Math.random() * 900000)
-                insertOTP({ mobile: trimmedNumber, otp: otp }, (error, results) => {
-                    if (error) {
-                        logger.error(error)
-                        return res.status(500).json({
-                            success: 0,
-                            message: "Database connection error"
-                        });
-                    }
-                    if (results) {
-                        axios.get(`https://sapteleservices.com/SMS_API/sendsms.php?username=Tmc_medicity&password=c9e780&sendername=TMDCTY&mobile=${mobileNumber}&template_id=1407162012178109509&message=Your+Medicity+App+OTP+code:+${otp}+DuHTEah22dE.Travancore+Medicity+.&routetype=1`).then((response) => {
-                            return res.status(200).json({
-                                success: 2,
-                                message: "OTP sent successfully"
-                            });
-                        }).catch((error) => {
-                            logger.error(error)
-                            return res.status(200).json({
-                                success: 3,
-                                message: "Error in sending OTP,Please try again"
-                            });
-                        })
-
-                    }
-                })
-            }
-        })
-    },
-    verifyOTPandLogin: async (req, res) => {
-        const body = req.body
-
-        verifyOTP(body, (error, results) => {
-            if (error) {
-                logger.error(error)
-                return res.status(500).json({
-                    success: 0,
-                    message: "Database connection error"
-                });
-            }
-            if (results.length === 0) {
-                return res.status(200).json({
-                    success: 1,
-                    message: "Incorrect OTP"
-                });
-            }
-            if (results.length > 0) {
-
-                const userData = results[0]
-                const { user_slno, name, login_type } = userData
-
-                const accessToken = generateAccessToken(userData);
-                const refreshToken = generateRefreshToken(user_slno);
-
-                // insert the refresh token
-                insertRefreshToken({ user_slno, refresh_token: refreshToken }, (error, results) => {
-
-                    if (error) {
-                        logger.error(error)
-                        return res.status(500).json({
-                            success: 0,
-                            message: "Database connection error"
-                        });
-                    }
-
-                    if (results) {
-
-                        const returnData = {
-                            user_slno,
-                            name,
-                            accessToken,
-                            login_type
-                        }
-
-                        res.cookie("accessToken", accessToken, {
-                            httpOnly: true,
-                            // secure: true,
-                            // maxAge: 900000, // 15 min       
-                            maxAge: 60000, // 1 min       
-                            sameSite: "strict",
-                        });
-
-                        // res.cookie("refreshToken", refreshToken, {
-                        //     httpOnly: true,
-                        //     secure: true,
-                        //     maxAge: 86400000, // 24 hour
-                        //     sameSite: "strict",
-                        // });
-
-                        res.json({
-                            success: 2,
-                            userInfo: JSON.stringify(returnData),
-                            message: "OTP verified successfully"
-                        });
-
-                    }
-                })
-            }
-        })
-    },
-    getRefershToken: (req, res) => {
-        const id = req.params.id
-        getRefershToken(id, (error, results) => {
-            if (error) {
-                logger.error(error)
-                return res.status(403).json({ message: "Invalid refresh token" });
-            }
-            if (results.length === 0) {
-                return res.status(403).json({ message: "Invalid refresh token" });
-            }
-            if (results.length > 0) {
-
-                // console.log(results[0].token)
-                const refreshToken = results[0].token;
-
-                jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-
-                    if (err) {
-                        return res.status(403).json({ message: "Invalid refresh token" });
-                    }
-
-                    const newAccessToken = generateAccessToken({
-                        id: id
-                    });
-                    // console.log(newAccessToken)
-                    res.cookie("accessToken", newAccessToken, {
-                        httpOnly: true,
-                        secure: true,
-                        // maxAge: 900000, // 15 min       
-                        maxAge: 60000, // 1 min       
-                        sameSite: "strict",
-                    });
-                });
-            }
-
-        })
-    }
-
-}
+          }
+        );
+      }
+    });
+  },
+};
