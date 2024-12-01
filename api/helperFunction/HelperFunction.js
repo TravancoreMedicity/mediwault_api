4db9a7d220b5f17404e84b3ec7cd630bd8d98324
ManagerTMC <itmanager@tmc.ac.in>
2024-12-01 15:55:14 +0530

    user managemnt

--- api/helperFunction/HelperFunction.js
+++ api/helperFunction/HelperFunction.js
@@ -4,11 +4,11 @@ const jwt = require("jsonwebtoken");
 const logger = require("../../logger/logger");
 
 const generateAccessToken = (userData) =>
-    jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "3m" });
+    jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30m" });
 
 const generateRefreshToken = ({ userSlno }) =>
     jwt.sign({ id: userSlno }, process.env.REFRESH_TOKEN_SECRET, {
-        expiresIn: "5m",
+        expiresIn: "1d",
     });
 
 module.exports = { generateAccessToken, generateRefreshToken };
